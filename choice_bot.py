e02085ab1471ae69eecb6c4e9f5de19f65db9d73
hotjuicew <hotjuicew@outlook.com>
2023-07-18 02:22:55 +0800

    feat: update random_number

--- choice_bot.py
+++ choice_bot.py
@@ -13,6 +13,7 @@ class ChoiceBot:
     def __init__(self, api_token):
         self.bot = telebot.TeleBot(api_token)
         self.bot.delete_webhook()
+        self.user_context = {}  # 用于存储用户的上下文数据
 
         @self.bot.message_handler(commands=['help', 'start'])
         def send_welcome(message):
@@ -24,15 +25,33 @@ class ChoiceBot:
             self.bot.send_message(message.chat.id, choice)
 
         @self.bot.message_handler(commands=['random'])
-        def random_number(message):
-            params = message.text.split()
-            if len(params) != 3:
-                self.bot.reply_to(message, "格式错误:\n/random <num1> <num2>，数字之间用空格隔开哦^o^")
-                return
-            a = int(params[1])
-            b = int(params[2])
-            number = random.randint(min(a, b), max(a, b))
-            self.bot.send_message(message.chat.id, f"我的选择是{number}")
+        def get_start_number(message):
+            self.bot.send_message(message.chat.id, "请输入起始数:")
+            self.bot.register_next_step_handler(message, get_end_number)
+
+        def get_end_number(message):
+            try:
+                start_number = int(message.text)
+
+                self.bot.send_message(message.chat.id, "请输入终止数:")
+                self.bot.register_next_step_handler(message, lambda msg: generate_random_number(msg, start_number))
+
+            except ValueError:
+                self.bot.send_message(message.chat.id, "无效的起始数，请输入一个整数")
+                self.bot.send_message(message.chat.id, "请输入起始数:")
+                self.bot.register_next_step_handler(message, get_start_number)
+
+        def generate_random_number(message, start_number):
+            try:
+                end_number = int(message.text)
+
+                number = random.randint(min(start_number, end_number), max(start_number, end_number))
+                self.bot.send_message(message.chat.id, f"我的选择是{number}")
+
+            except ValueError:
+                self.bot.send_message(message.chat.id, "无效的终止数，请输入一个整数")
+                self.bot.send_message(message.chat.id, "请输入终止数:")
+                self.bot.register_next_step_handler(message, lambda msg: generate_random_number(msg, start_number))
 
         @self.bot.message_handler(commands=['dosth'])
         def dosth(message):
