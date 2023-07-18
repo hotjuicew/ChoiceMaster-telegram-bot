c62f52ec5ad26bbaea4093cada5c388501f165f4
hotjuicew <hotjuicew@outlook.com>
2023-07-19 00:57:44 +0800

    fix: declare encoding

--- choice_bot.py
+++ choice_bot.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
+# coding=utf-8
 
-# This is a simple echo bot using the decorator mechanism.
-# It echoes any incoming text messages.
 import random
 import telebot
 from telebot import types
