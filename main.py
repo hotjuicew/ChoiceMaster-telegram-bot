21b03c328641e257c26e0201251368d3d900b1f2
hotjuicew <hotjuicew@outlook.com>
2023-07-22 15:20:20 +0800

    fix: remove peoxy config

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
 from os.path import exists
-from telebot import apihelper
 from choice_bot import ChoiceBot
 from dotenv import load_dotenv
 
@@ -13,7 +12,4 @@ if not api_token:
     api_token = os.environ.get('API_TOKEN')
 if __name__ == '__main__':
     bot = ChoiceBot(api_token)
-    http_proxy = os.environ.get('HTTP_PROXY')
-    if http_proxy:
-        apihelper.proxy = {'http': http_proxy}
     bot.start()
